ed4511cb4c5cb641a92546999a43bd25e1adbbbe
SeaJungg <wantedsaea@likelion.org>
2021-05-30 23:48:59 +0900

    relu

--- relu.s
+++ relu.s
@@ -11,20 +11,21 @@
 # ==============================================================================
 relu:
     # Prologue
-
+    add t0, x0, x0
 
 loop_start:
+    beq t0, a1, loop_end
+    addi t2, x0, 4
+    mul t1, t0, t2
+    add t2, t1, a0
+    lw t3, 0(t2)
+    bge t3, x0, loop_continue # if t3>0, go to loop_continue
+    add t3, x0, x0
+    sw t3, 0(t2)
     
-
-
-
-
-
-
-
 loop_continue:
-
-
+    addi t0, t0, 1
+    j loop_start
 
 loop_end:
 
